f10bc388cde06d6d457d4ed4bc21881972440b2a
JuanJo0612 <juanjo.solanope@ecci.edu.co>
2024-10-10 19:50:18 -0500

    empezamos a subir todos los archivos y codigos

---
+++ primera clase.py
@@ -0,0 +1,35 @@
+print ("HOLA MUNDOOOO :D")
+num = 123
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
+
+num= cociente
+residuo = num % 2
+cociente = num//2
+print( "res:", residuo, "coc:", cociente)
