9a133dc5b32e502fb37f580b09e2ae7bfa0637fb
JuanJo0612 <juanjo.solanope@ecci.edu.co>
2024-11-21 22:56:44 -0500

    Subiendo otra parte del archivo :D

---
+++ Ejercicios externos/Converitr flotantes o Str.py
@@ -0,0 +1,18 @@
+numero : int = 10
+print(numero)
+print(type(numero))
+
+nuero = 10.1
+print(nuero)
+print(int(nuero))
+
+numero = "10"
+print(numero, int(numero))
+print(type(numero))
+
+num1:int = input("escriba un numero: ")
+num2 = input(" esribid otro numero: ")
+print("usted escribio ", num1, "y", num2)
+
+suma = num1 + num2
+print(" la suma de los numeros es: ", suma)
